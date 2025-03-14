a07813a455d63ad9afc3ccfde0cc6c3e2c0b6ca0
Arjun Mohunta <amohunta@uci.edu>
2025-03-14 23:58:42 +0000

    try

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -66,14 +66,18 @@ void load_words(set<string>& word_list, const string& file_name) {
 
 void print_word_ladder(const vector<string>& ladder) {
     if (ladder.empty()) {
-        cout << "No word ladder found." << endl;
+        cout << "No word ladder found.\n";
         return;
     }
+
+    cout << "Word ladder found: ";
     for (size_t i = 0; i < ladder.size(); ++i) {
         cout << ladder[i];
-        if (i < ladder.size() - 1) cout << " -> ";
+        if (i < ladder.size() - 1) {
+            cout << " ";
+        }
     }
-    cout << endl;
+    cout << " \n";
 }
 
 void verify_word_ladder() {
